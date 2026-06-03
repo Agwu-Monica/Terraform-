709335d9577cf79bc1eb276f158d7f5897d1097a
Agwu-Monica <monichanny@gmail.com>
2026-06-03 12:53:20 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  client_id       = "5f167295-b51e-4b85-a929-0ea38adeae50"
+  client_secret   = "T0H8Q~.mCWtVH1EQi4BdGUWs.V8BfObR6mTfebI0"
+  tenant_id       = "cf14408a-6f1c-4108-bec5-d24281f2ecc3"
+  subscription_id = "b8b5cc95-0e28-4f85-9578-5f7393b250d6"
 }
